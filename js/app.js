53bce33be2f6339dafc7b52d226f49c0da170efc
Russ Nealis <codemunkee@gmail.com>
2017-10-22 18:59:56 -0700

    now with a cat object

--- js/app.js
+++ js/app.js
@@ -1,20 +1,26 @@
-var ViewModel = function() {
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/cat1.jpeg');
     this.imgAttribution = ko.observable('https://static.pexels.com/photos/127028/pexels-photo-127028.jpeg');
+    this.nickNames = ko.observableArray(['Billy', 'Barry', 'Bruce']);
 
-    this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
-    };
-
-    this.level = ko.computed(function() {
+    var level = ko.computed(function() {
         if (this.clickCount() < 10) {
             return 'Baby';
         } else {
             return 'Not a Baby';
         }
     }, this);
+};
+
+var ViewModel = function() {
+
+    this.currentCat = ko.observable( new Cat() );
+
+    this.incrementCounter = function() {
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    };
 
 };
 
