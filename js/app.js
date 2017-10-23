5cb72ca39ed479f398f10161420eb222a485b3a7
Russ Nealis <codemunkee@gmail.com>
2017-10-23 09:17:50 -0700

    almost working

--- js/app.js
+++ js/app.js
@@ -32,20 +32,30 @@ var initialCats = [
     }
 ];
 
+// [ ] Make the cats show up in a list
+//
+// [ ] Make the currentCat change when you click on a cat in the list
+
 var ViewModel = function() {
 
     var self = this;
 
-    this.catList = new ko.observableArray([]);
+    self.catList = new ko.observableArray([]);
 
     initialCats.forEach(function(catItem) {
         self.catList.push(new Cat(catItem));
     });
 
-    this.currentCat = ko.observable(this.catList()[0]);
+    self.currentCat = ko.observable(self.catList()[0]);
 
-    this.incrementCounter = function() {
+    self.incrementCounter = function() {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    };
+
+    self.setCurrentCat = function() {
+        console.log(self.currentCat());
+        self.currentCat = ko.observable(this);
+        console.log(self.currentCat());
     };
 
 };
